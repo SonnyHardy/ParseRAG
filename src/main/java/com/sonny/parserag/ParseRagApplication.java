2ef8e61d206393643674870d9b241ada587b50b2
SonnyHardy <htambeck@gmail.com>
2026-05-01 12:35:31 +0200

    Initial project

---
+++ src/main/java/com/sonny/parserag/ParseRagApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.parserag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseRagApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ParseRagApplication.class, args);
+    }
+
+}
